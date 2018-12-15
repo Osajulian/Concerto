e4e1285f918f6ff8e73f4b5ccf99f496688d4ed3
Osajulian <you@example.com>
2018-12-15 12:47:26 -0500

    components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import Menu from "./components/MenuComponent";
+import { DISHES } from './shared/dishes';
 import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    }
+  }
   render() {
     return (
       <div className="App">
         <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrand href="/">Ojuelegba</NavbarBrand>
+            <NavbarBrand href="/">Ojuelegba Restaurant</NavbarBrand>
           </div>
         </Navbar>
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
